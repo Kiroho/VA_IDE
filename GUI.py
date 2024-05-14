a14dfc9ac4f99bfe93fdd04c13e4365fe37bb2b7
Rene Wentzel <59712322+Kiroho@users.noreply.github.com>
2024-05-14 03:54:04 +0200

    Added Microphone option + voice to text

--- GUI.py
+++ GUI.py
@@ -1,36 +1,155 @@
+import threading
+import tkinter
 from tkinter import *
+from tkinter.filedialog import asksaveasfilename, askopenfilename
 import subprocess
-
-main_window = Tk()
-main_window.title("Placeholder")
-
-
-def run():
-    # command = f'python{file}'
-    # process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-    code = editor.get("1.0", END)
-    exec(code)
+from Microphone_Selection import MicrophoneWindow
+from Voice_Assistant import VoiceAssistant
+import pyaudio
 
 
-menu_bar = Menu(main_window)
+class MainGUI(tkinter.Tk):
+    def process_va_data(self, data):
+        print(data)
+    def __init__(self):
+        tkinter.Tk.__init__(self)
+        self.file_path = ""
+        self.process = ''
+        self.py_audio = None
+        self.microphone_data = ''
+        self.connected_devices = {}
+        self.va_on_off = False
+        self.va = VoiceAssistant(owner=self)
 
-file_bar = Menu(menu_bar, tearoff=0)
-file_bar.add_command(label="New")
-file_bar.add_command(label="Save")
-file_bar.add_command(label="Save As")
-file_bar.add_command(label="Load")
-menu_bar.add_cascade(label="File", menu=file_bar)
+        def voice_assistant():
+            if not self.va_on_off:
+                print("Voice Assistant is now ON")
+                t = threading.Thread(target=self.va.start)
+                t.daemon = True
+                t.start()
+            else:
+                print("Voice Assistant is now OFF")
+                self.va.stop()
+            self.va_on_off = not self.va_on_off
 
-option_bar = Menu(menu_bar, tearoff=0)
-option_bar.add_command(label="Microphone")
-option_bar.add_command(label="Hardware Acceleration")
-menu_bar.add_cascade(label="Options", menu=option_bar)
 
-menu_bar.add_command(label="Run", command=run)
 
-main_window.config(menu=menu_bar)
+        def select_microphone():
+            check_devices()
+            MicrophoneWindow(self)
 
-editor = Text()
-editor.pack()
+        def check_devices():
+            self.connected_devices = {}
+            if self.py_audio is None:
+                self.py_audio = pyaudio.PyAudio()
+            else:
+                self.py_audio.terminate()
+                self.py_audio = pyaudio.PyAudio()
+            device_count = self.py_audio.get_device_count()
+            for i in range(device_count):
+                # Get the device info
+                device_info = self.py_audio.get_device_info_by_index(i)
+                # Check if this device is a microphone (an input device)
+                if device_info.get('maxInputChannels') > 0:
+                    # print(f"Microphone: {device_info.get('name')} , Device Index: {device_info.get('index')}")
+                    self.connected_devices[device_info.get('name')] = device_info.get('index')
+            # print("\n\n\n")
+            # print(self.connected_devices)
 
-main_window.mainloop()
+        def set_file_path(path):
+            self.file_path = path
+
+        def new_file():
+            set_file_path("")
+            editor.delete("1.0", END)
+            console.delete("1.0", END)
+
+        def open_file():
+            path = askopenfilename(filetypes=[('Python Files', '*.py')])
+            if path.endswith('py'):
+                with (open(path, 'r') as file):
+                    code = file.read()
+                    editor.delete("1.0", END)
+                    editor.insert("1.0", code)
+                    console.delete("1.0", END)
+                    set_file_path(path)
+            else:
+                print("Fehlermeldung: Es handelt sich nicht um eine Python Datei.")
+
+        def save_as():
+            path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+            if not path.endswith('py'):
+                path += '.py'
+            if not path == '':
+                with open(path, 'w') as file:
+                    code = editor.get("1.0", END)
+                    file.write(code)
+                    set_file_path(path)
+
+        def save():
+            if self.file_path == "":
+                path = asksaveasfilename(filetypes=[('Python Files', '*.py')])
+                if not path.endswith('py') and not path == '':
+                    path += '.py'
+            else:
+                path = self.file_path
+            if not path == '':
+                with open(path, 'w') as file:
+                    code = editor.get("1.0", END)
+                    file.write(code)
+                    set_file_path(path)
+
+        def run():
+            t = threading.Thread(target=run_code)
+            t.daemon = True
+            t.start()
+
+        def run_code():
+            save()
+            global process
+            # command = f'python{file_path}'
+            process = subprocess.Popen(self.file_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            output, error = process.communicate()
+            console.insert(END, output)
+            console.insert(END, error)
+            print(output)
+            print(error)
+            print(self.file_path)
+
+        def stop():
+            console.insert(END, "ToDo: Implement Stop\n")
+
+        menu_bar = Menu(self)
+
+        file_bar = Menu(menu_bar, tearoff=0)
+        file_bar.add_command(label="New", command=new_file)
+        file_bar.add_command(label="Open", command=open_file)
+        file_bar.add_command(label="Save", command=save)
+        file_bar.add_command(label="Save As", command=save_as)
+        file_bar.add_command(label="Exit", command=exit)
+        menu_bar.add_cascade(label="File", menu=file_bar)
+
+        option_bar = Menu(menu_bar, tearoff=0)
+        option_bar.add_command(label="Microphone", command=select_microphone)
+        option_bar.add_command(label="Hardware Acceleration")
+        menu_bar.add_cascade(label="Options", menu=option_bar)
+
+        menu_bar.add_command(label="Run", command=run)
+        menu_bar.add_command(label="Stop", command=stop)
+        menu_bar.add_command(label="Voice", command=voice_assistant)
+
+        self.config(menu=menu_bar)
+
+        editor = Text()
+        editor.pack()
+
+        console = Text(height=10)
+        console.pack()
+
+
+def main():
+    MainGUI().mainloop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Voice_Assistant.py
@@ -0,0 +1,115 @@
+import threading
+
+from faster_whisper import WhisperModel
+import pyaudio
+import wave
+import os
+import keyboard
+import time
+import audioop
+
+
+def check_volume(data):
+    rms = audioop.rms(data, 2)
+    return rms
+
+
+class VoiceAssistant:
+
+    def __init__(self, owner):
+        self.stop_all = False
+        self.owner = owner
+        self.start_time = 0.0
+        self.end_time = 0.0
+        self.voice_time_start = 0.0
+        self.voice_time_end = 0.0
+
+        self.py_audio = pyaudio.PyAudio()
+        self.input_device_index = "0"
+        self.temp_audio_file_path = "temp/temp_chunk.wav"
+        self.temp_log_file_path = ""
+        self.volume_start_threshold = 1000
+        self.volume_stop_threshold = 500
+        self.recording_timeout = 2
+
+        self.tts_device = "cuda"
+        self.tts_compute_type = "float32"
+        print("Model stuff....\n")
+        self.model = WhisperModel("medium", device=self.tts_device, compute_type=self.tts_compute_type)
+        print("Model stuff done!\n")
+
+    def stop(self):
+        self.stop_all = True
+        # self.py_audio.terminate()
+
+    def start(self):
+        self.stop_all = False
+        self.py_audio = pyaudio.PyAudio()
+        self.main()
+
+    def bitstream_to_wave(self, frames, path):
+        wf = wave.open(path, 'wb')
+        wf.setnchannels(1)
+        wf.setsampwidth(self.py_audio.get_sample_size(pyaudio.paInt16))
+        wf.setframerate(16000)
+        wf.writeframes(b''.join(frames))
+        wf.close()
+
+    def recording(self, stream):
+        frames = []
+        self.voice_time_start = time.time()
+        print("recording:...\n")
+        while not self.stop_all:
+            data = stream.read(1024)
+            frames.append(data)
+            if check_volume(data) < self.volume_stop_threshold:
+                self.voice_time_end = time.time()
+                deltatime = self.voice_time_end - self.voice_time_start
+                if deltatime > self.recording_timeout:
+                    self.start_time = time.time()
+                    break
+        print("recording ended")
+        self.bitstream_to_wave(frames, self.temp_audio_file_path)
+
+    def main(self):
+        self.stop_all = False
+        stream = self.py_audio.open(format=pyaudio.paInt16, channels=1, rate=16000,
+                                    input_device_index=int(self.input_device_index), input=True, frames_per_buffer=1024)
+        log = ""
+        try:
+            while not self.stop_all:
+                if check_volume(stream.read(1024)) > 1000:
+                    self.recording(stream)
+                    print("processing...")
+                    segments, info = self.model.transcribe(self.temp_audio_file_path, beam_size=5, language="de",
+                                                           condition_on_previous_text=False)
+                    text = ""
+                    for segment in segments:
+                        text += segment.text + " "
+                        # print(segment.text)
+                    log += text
+                    print("processing done")
+
+                    if self.owner:
+                        print("Call owner")
+                        self.owner.process_va_data(text)
+
+                    self.end_time = time.time()
+                    deltatime = self.end_time - self.start_time
+                    print("Procession Time: ")
+                    print(deltatime)
+                    os.remove(self.temp_audio_file_path)
+        except Exception:
+            with open("temp/log.txt", "w") as log_file:
+                log_file.write(log)
+        finally:
+            print("Stopping...")
+            print("LOG: " + log)
+            stream.stop_stream()
+            stream.close()
+            # self.py_audio.terminate()
+
+# if __name__ == '__main__':
+#     print("Start")
+#     start = VoiceAssistant()
+#     start.start()
